6ac5e702a26806cb028096067b283d99cb6652a7
杨刚 <zhanghongqin@interrcs.com>
2019-06-16 16:23:00 +0800

    * first commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"io"
+)
+
+func main(){
+	fmt.Println("weasel main start!")
+	//server.Run()
+	test()
+}
+
+func test(){
+	f, err := os.Open("/Users/ycysvler/Documents/aba.txt")
+
+	if err != nil {
+		fmt.Println("os Open error: ", err)
+		return
+	}
+	defer f.Close()
+
+	br := bufio.NewReader(f)
+	for {
+		line, _, err := br.ReadLine()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Println("br ReadLine error: ", err)
+			return
+		}
+
+		fmt.Printf("%s\n", string(line))
+		//fmt.Println("line: ", string(line))
+	}
+
+}
---
+++ server/server.go
@@ -0,0 +1,18 @@
+package server
+
+import "github.com/gin-gonic/gin"
+
+func Run(){
+	// 设置 gin 的模式（调试模式：DebugMode, 发行模式：ReleaseMode）
+	gin.SetMode(gin.DebugMode)
+	// 创建一个不包含中间件的路由器
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+
+}
